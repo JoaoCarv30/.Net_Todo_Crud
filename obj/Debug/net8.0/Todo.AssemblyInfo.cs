7f6e911d2a79cafb4044eaf2a0453e6aacf1c3d4
João Carvalho <Carvalhoestudante1@gmail.com>
2025-03-24 20:15:07 -0300

    database string configuration OK

--- obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994c0e1ead3eed76f1d2a6df1a5400138e7f390d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4bea1f7fe00edc1ab0694d269da41df4effec2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
